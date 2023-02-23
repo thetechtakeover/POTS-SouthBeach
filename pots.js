2c6fa161e066b4fa06346fb29d23e436f1c7c536
Evan Poole <thetechtakeover@gmail.com>
2023-02-22 19:54:50 -0500

    Changed Slogan, Clean up comments moved sections

---
+++ pots.js
@@ -0,0 +1,13 @@
+
+let name = prompt('Hello POTS guest, What is your name?');
+
+alert(`Thank you for visiting us ${name}!`);
+
+let firstTimeHere = prompt(`${name} is this your first time visiting?`);
+
+let visiting = true;
+if (visiting === firstTimeHere) {
+    alert(`We are glad to have you. We hope you enjoy your stay and we appreciate your business!`);
+} else {
+    alert (`Welcome Back ${name}! We thank you for visting POTS again!`);
+}
